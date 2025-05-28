8cdf77a5db21f3e5981e93986e90ea6882791bae
Dominique Jeldes <1121623@est.intec.edu.do>
2025-05-28 14:20:52 -0400

    actuali

---
+++ Teoria/Pilas(Stacks)/Pilas(Stacks).cpp
@@ -0,0 +1,51 @@
+/*
+-------------------------------------------------------------------------------------
+Tarea: #2 - Pilas (Stacks)
+Materia: IDS343-01-ESTRUCTURAS DE DATOS Y ALGORITMOS I (6-8pm)
+-------------------------------------------------------------------------------------
+Una pila (stack) es una estructura de datos en donde el último en entrar es el primero en salir.
+Construir un programa C++ que simule una pila, utilizando una estructura de datos como la que
+sigue para los nodos:
+
+struct Pila {
+ int dato;
+ Pila *next;
+};
+
+RESTRICCIONES:
+A. La pila debe poder realizar las operaciones de Push y Pop.
+B. Tener en consideración de desplegar un mensaje de “Empty Stack”, cuando se trate de
+hacer un Pop cuando la pila este vacía.
+C. El programa debe tener un menú para realizar las operaciones de Push, Pop y Desplegar
+toda la pila realizando Pops hasta el último nodo. Además, debe tener una opción de
+salir del programa.
+
+Nota: Este programa valida correctamente las entradas del usuario y maneja escenarios de errores para garantizar su robustez.
+
+INTEGRANTES (Grupo 6):
+    Samira Vasquez - 1125467
+    Dominique Jeldes - 1121623
+    Enmanuel Carrasco - 1124404
+    Juan Castillo - 1127310
+    Sebastian Ventura - 1125467
+    Elianyer Gomez - 1118021
+
+Fecha: 28/05/2025
+
+*/
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+
+    //Funcion pincipal
+
+    //
+
+    //Fin lol
+    return 0;
+
+}
