56a4e1479ae51ca23ff90251f301771dae2745a9
Dominique Jeldes <1121623@est.intec.edu.do>
2025-06-22 00:36:22 -0400

    Completo

--- Laboratorio/6-Serie Fibonacci/6-Serie Fibonacci.cpp
+++ Laboratorio/6-Serie Fibonacci/6-Serie Fibonacci.cpp
@@ -3,7 +3,7 @@
 TAREA: Serie Fibonacci  (LABORATORIO)
 Materia: IDS343L-01-LABORATORIO ESTRUCTURAS DE DATOS Y ALGORITMOS I
 -----------------------------------------------------------------------------------------
-Descripción:
+DescripciÃ³n:
     Las series de Fibonacci son sucesiones de numeros que reflejan una interesante relacion.
     Constan del conjunto ordenado de cifras cuyo valor se forma con la suma de los dos numeros anteriores,
     lo que da origen a otras relevantes inferencias matematicas.
@@ -12,7 +12,6 @@ INTEGRANTE:
     Dominique Jeldes - 1121623
 
 Fecha: 20/Jun/2025
-
 */
 #include <iostream>
 
