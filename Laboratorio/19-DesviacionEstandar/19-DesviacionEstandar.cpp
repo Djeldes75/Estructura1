9234e5eb6b10cff4e2dbec9f65f0d9ff6dec7bb8
Dominique Jeldes <1121623@est.intec.edu.do>
2025-07-04 19:58:00 -0400

    Update 19-DesviacionEstandar.cpp

--- Laboratorio/19-DesviacionEstandar/19-DesviacionEstandar.cpp
+++ Laboratorio/19-DesviacionEstandar/19-DesviacionEstandar.cpp
@@ -1,20 +1,47 @@
-// 19-DesviacionEstandar.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+
+*/
 
 #include <iostream>
+#include <cmath>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Ingrese la cantidad de valores: ";
+    cin >> n;
+
+    // Crear arreglo dinámico
+    double* datos = new double[n];
+    double suma = 0.0;
+
+    // Entrada de datos
+    cout << "Ingrese los " << n << " valores:" << endl;
+    for (int i = 0; i < n; ++i) {
+        cin >> datos[i];
+        suma += datos[i];
+    }
+
+    // Calcular la media
+    double media = suma / n;
+
+    // Calcular varianza
+    double sumaCuadrados = 0.0;
+    for (int i = 0; i < n; ++i) {
+        sumaCuadrados += pow(datos[i] - media, 2);
+    }
+
+    double varianza = sumaCuadrados / (n - 1);
+    double desviacionEstandar = sqrt(varianza);
+
+    // Mostrar resultados
+    cout << "Media: " << media << endl;
+    cout << "Varianza: " << varianza << endl;
+    cout << "Desviacion estandar: " << desviacionEstandar << endl;
+
+    // Liberar memoria
+    delete[] datos;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
