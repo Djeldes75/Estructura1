670c67640c32d9840772dc1571a98bca1fc31efc
Dominique Jeldes <1121623@est.intec.edu.do>
2025-07-04 19:36:23 -0400

    Update LaHora.cpp

--- Laboratorio/15-LaHora/LaHora.cpp
+++ Laboratorio/15-LaHora/LaHora.cpp
@@ -4,7 +4,7 @@ Tarea: La Hora
 Materia: IDS343-01-L LAB. ESTRUCTURAS DE DATOS Y ALGORITMOS I
 -------------------------------------------------------------------------------------------------------
 Descripcion:
-   La hora es un número de cuatro dígitos, donde los dos primeros representan la hora y los dos últimos los minutos.
+   La hora es un nÃºmero de cuatro dÃ­gitos, donde los dos primeros representan la hora y los dos Ãºltimos los minutos.
 
  Autor:
     Dominique Jeldes - 1121623
@@ -14,28 +14,20 @@ Fecha: 25/Jun/2025
 */
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
-    for (int hora = 0; hora < 24; hora++) {
-        for (int minuto = 0; minuto < 60; minuto++) {
-            int h1 = hora / 10;
-            int h2 = hora % 10;
-            int m1 = minuto / 10;
-            int m2 = minuto % 10;
-
-            int suma = h1 * h1 + h2 * h2 + m1 * m1 + m2 * m2;
-            int hhmm = hora * 100 + minuto;
-
-            if (suma == hhmm) {
+    for (int h = 1; h < 24; h++) { // desde 1 para evitar 00:00
+        for (int m = 0; m < 60; m++) {
+            int hhmm = h * 100 + m;
+            if (h * h + m * m == hhmm) {
                 cout << "La hora es: ";
-                if (hora < 10) cout << "0";
-                cout << hora << ":";
-                if (minuto < 10) cout << "0";
-                cout << minuto << endl;
+                cout << setfill('0') << setw(2) << h << ":"
+                    << setfill('0') << setw(2) << m << endl;
+                return 0;
             }
         }
     }
-
     return 0;
 }
